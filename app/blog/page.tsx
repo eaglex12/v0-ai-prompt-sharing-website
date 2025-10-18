e84244b5f4f0ddd46ca7434b73dd60d8606ad800
DIVYANSH SINGH RATHORE <115221626+eaglex12@users.noreply.github.com>
2025-10-18 15:12:35 +0530

    minor fix

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -63,7 +63,7 @@ export default async function Page() {
 				type="application/ld+json"
 				dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
 			/>
-			<Suspense fallback={<div>Loading blog...</div>}>
+			<Suspense fallback={null}>
 				<BlogPage 
 					initialPosts={blogPosts}
 					initialCategories={blogCategories}
